1150e07e69b79d66c22f141706eae6a4b01dd7c9
Santiago Rodriguez <santiara010@gmail.com>
2024-09-11 13:56:11 -0300

    agregue los componentes y un estilo al titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
+import Titulo from './components/Titulo'
 
 function App() {
-
   return (
-    
+    <Titulo></Titulo>
   )
 }
 
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,10 @@
+
+const Titulo = () => {
+    return (
+        <section>
+            <h1 className="color">Hello world: </h1>
+        </section>
+    );
+};
+
+export default Titulo;
